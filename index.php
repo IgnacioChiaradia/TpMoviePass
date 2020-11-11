e98bd4deee9932ce7f6b3dcbd25a34ac7e3ad852
Andres Lerner <38118136+AndresL99@users.noreply.github.com>
2020-11-11 18:03:49 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -12,13 +12,24 @@
 	use Config\Request 	as Request;
 		
 	Autoload::Start();
+		
+	//$genres = file_get_contents('https://api.themoviedb.org/3/genre/movie/list?api_key=6fef728cda826ca3e07c760bb8960523&language=en-US');
+	
+
+
 	
 	session_start();
 	//session_destroy();
+		
+	//$genres = file_get_contents('https://api.themoviedb.org/3/genre/movie/list?api_key=6fef728cda826ca3e07c760bb8960523&language=en-US');
+	
+	//$movies = file_get_contents('https://api.themoviedb.org/3/movie/990?api_key=' . TMDB_KEY . '&language=en-US');
+	
 
 	require_once(VIEWS_PATH."header.php");
 
 	//require_once(VIEWS_PATH."nav.php");
+
 
 	Router::Route(new Request());
 
